9477ca14559b14844fc029698786fb15fb700361
Lukong Isaac <lisaacjunior@yahoo.com>
2023-04-05 03:57:35 +0100

    remove service folder

--- spec/services/json_parser.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe Parser::JsonParser, type: :service do
-  describe "responds to methods" do
-    it "should respodn to to_snake_case" do
-      expect(described_class.respond_to?(:to_snake_case).to be_true )
-    end
-  end
-end
